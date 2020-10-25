d46232c9b40270cf4ee2dca92af6467bce930d0c
Sourav Sahu <sahu.souravrb1999@gmail.com>
2020-10-25 18:52:22 +0530

    Objective 3
    time program in server

---
+++ 3/serverTime.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <time.h>
+int main()
+{
+    struct sockaddr_in sa;
+    struct sockaddr_in cli;
+    int sockfd, conntfd;
+    int len, ch;
+    char str[100];
+    time_t tick;
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+    {
+        printf("error in socket\n");
+        exit(0);
+    }
+    else
+        printf("Socket opened");
+    bzero(&sa, sizeof(sa));
+    sa.sin_port = htons(5600);
+    sa.sin_addr.s_addr = htonl(0);
+    if (bind(sockfd, (struct sockaddr *)&sa, sizeof(sa)) < 0)
+    {
+        printf("Error in binding\n");
+    }
+    else
+        printf("Binded Successfully");
+    listen(sockfd, 50);
+    for (;;)
+    {
+        len = sizeof(ch);
+        conntfd = accept(sockfd, (struct sockaddr *)&cli, &len);
+        printf("Accepted");
+        tick = time(NULL);
+        snprintf(str, sizeof(str), "%s", ctime(&tick));
+        printf("%s", str);
+        write(conntfd, str, 100);
+    }
+}
